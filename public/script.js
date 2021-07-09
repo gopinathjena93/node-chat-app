9bec4096aa7e784ec75d72dba90875bbb2acd53a
gopinathjena93 <70263878+gopinathjena93@users.noreply.github.com>
2021-07-09 12:46:30 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {	
-	/*localStorage.removeItem("username");
+	localStorage.removeItem("username");
 	
 	var path = window.location.pathname;
 	var page = path.split("/").pop();
 
-	const googleTockenId = localStorage.getItem("googleTockenId");
+    if(page == "chatroom") onLoad();
+
+	/*const googleTockenId = localStorage.getItem("googleTockenId");
     const name = localStorage.getItem("name");
     const email = localStorage.getItem("email");
     const Image = localStorage.getItem("Image");  
@@ -22,16 +24,16 @@ $(document).ready(function() {
 /***** added 08-07-2021    **********/
 
 
-function onSignIn(googleUser) {
+/*function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
     console.log(profile);
-    /*console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
     console.log('Full Name: ' + profile.getName());
     console.log('Given Name: ' + profile.getGivenName());
     console.log('Family Name: ' + profile.getFamilyName());
     console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());*/
+    console.log("Email: " + profile.getEmail());
 
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
@@ -49,7 +51,7 @@ function onSignIn(googleUser) {
         localStorage.setItem("Image", Image);
         window.location.href="/chatroom";
     }
-}
+}*/
 
  function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
@@ -78,6 +80,12 @@ function onSignIn(googleUser) {
     	localStorage.setItem("Image", Image);
     	window.location.href="/chatroom";
     }
+}
+
+function onLoad() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init();
+    });
 }
 
 function signOut() {
